9c7c02ab318bf0e1e1c981a3c7b5328b8880333f
hoangdvhp99 <hoangdv.hp99@gmail.com>
2020-04-15 01:25:33 +0700

    Add BT Tuan 7

---
+++ Tuan 7/GiaoTiep_ServerClient/Client/Client.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SimpleTCP;
+namespace Client
+{
+    public partial class Client : Form
+    {   
+        public Client()
+        {
+            InitializeComponent();
+        }
+        SimpleTcpClient client;
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            btnConnect.Enabled = false;
+            try
+            {
+                client.Connect(txtHost.Text, int.Parse(txtPort.Text));
+                client.WriteLine("Client connected!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString());
+                btnConnect.Enabled = true;
+            } 
+        }
+
+        private void Client_Load(object sender, EventArgs e)
+        {
+            client = new SimpleTcpClient();
+            client.StringEncoder = Encoding.UTF8;
+            client.DataReceived += Client_DataReceived;
+            
+        }
+
+        private void Client_DataReceived(object sender, SimpleTCP.Message e)
+        {
+            lbStatus.Invoke((MethodInvoker)delegate ()
+            {
+                lbStatus.Items.Add(e.MessageString);
+                if (e.MessageString == "Good Bye!\u0013"||e.MessageString== "Server is closed!\u0013") btnConnect.Enabled = true;
+            });
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            if (txtMessage.Text != "")
+            {
+                lbStatus.Items.Add("You said: " + txtMessage.Text);
+                client.WriteLineAndGetReply(txtMessage.Text, TimeSpan.FromSeconds(2));
+            }
+            txtMessage.Clear();
+        }
+    }
+}
---
+++ Tuan 7/GiaoTiep_ServerClient/GiaoTiepClientServer/Form1.cs
@@ -0,0 +1,99 @@
+﻿using SimpleTCP;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+
+namespace GiaoTiepClientServer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SimpleTcpServer server;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            server = new SimpleTcpServer();
+            server.Delimiter = 0x13;
+            server.StringEncoder = Encoding.UTF8;
+            server.DataReceived += Server_DataReceived;
+        }
+        
+        private string reply(string st)
+        {
+            string s="";
+            for(int k=0;k<st.Length-1;k++)
+            {
+                if ((st[k] < '0') || (st[k] > '9')) return "Xin chao, " + txtHost.Text + ":" + txtPort.Text;
+                switch (st[k])
+                {
+                    case '1': s+= "One "; break;
+                    case '2': s+= "Two "; break;
+                    case '3': s+= "Three "; break;
+                    case '4': s+= "Four "; break;
+                    case '5': s+= "Five "; break;
+                    case '6': s+= "Six "; break;
+                    case '7': s+= "Seven "; break;
+                    case '8': s+= "Eight "; break;
+                    case '9': s+= "Nine "; break;
+                    case '0': s += "Zero "; break; 
+                }
+            }
+            return s; 
+        }
+        bool done = false;
+        private void Server_DataReceived(object sender, SimpleTCP.Message e)
+        {
+            lbStatus.Invoke((MethodInvoker) delegate() 
+            {
+                lbStatus.Items.Add("+Client:" + e.MessageString);
+                if (e.MessageString == "END\u0013")
+                {
+                    e.ReplyLine("Good Bye!"); 
+                    done = true;
+                   // server.Stop();
+                }
+                else
+                    e.ReplyLine(string.Format("Server: {0}", reply(e.MessageString)));
+            });
+            if (done)
+            {
+                btnStop.Enabled = false;
+                btnStart.Enabled = true;
+                server.Stop();
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            btnStart.Enabled = false;
+            btnStop.Enabled = true;
+            lbStatus.Items.Add("Server is starting...");
+            IPAddress ip;
+            ip = IPAddress.Parse(txtHost.Text);
+            server.Start(ip,int.Parse(txtPort.Text));
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            btnStart.Enabled = true;
+            btnStop.Enabled = false;
+            if (server.IsStarted)
+            {
+                lbStatus.Items.Add("Server is closed!");
+                server.BroadcastLine("Server is closed!");
+                server.Stop();
+            }
+        }
+    }
+}
